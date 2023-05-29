f9e132361a96e63a385f6f5bf23eafa952af389e
DESKTOP-T5MELBJ\daffa <daffathh0800@gmail.com>
2023-05-29 12:12:07 +0700

    validate status and content

--- cypress/integration/pokeapi/headers.test.js
+++ cypress/integration/pokeapi/headers.test.js
@@ -1,5 +1,4 @@
 describe('validate headers ', () => {
-    
     it('Successfully validate headers content-type', () => {
         cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('pokemon')
         cy.get('@pokemon').its('headers').its('content-type')
@@ -8,10 +7,23 @@ describe('validate headers ', () => {
 
     it('Validate response body has objects', () =>{
         cy.request('Get', 'https://pokeapi.co/api/v2/pokemon/ditto').then((response) => {
-            expect(response.status).equal(200)
             expect(response.body).to.have.property('name')
-            //expect(response.body).to.have.property('url')
             expect(response.body.ability).is.not.null
-        });
-    });
+    })})
+    it('Validate status code', () => {
+        cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('pokemon')
+        cy.get('@pokemon').its('status').should('equal', 200)
+    })
+
+    it('Successfully Validate content', () => {
+        cy.request('https://pokeapi.co/api/v2/pokemon/bulbasaur').as('bulbasaur')
+        cy.get('@bulbasaur').its('body').should('include', {name : "bulbasaur"})
+    })
+
+    it('Validate response body has another objects', () =>{
+        cy.request('Get', 'https://pokeapi.co/api/v2/pokemon/ditto').then((response) => {
+            expect(response.body).to.have.property('name')
+            expect(response.body.name).to.eq('ditto')
+    })})
+    
 })
